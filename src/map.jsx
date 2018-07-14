a76eb80c416f045925e69c880ba6b916e7b1e8fd
rossmcarthur <r.mcarth20@gmail.com>
2018-07-14 17:24:08 -0400

    Add polygon draw basic functionality

--- src/map.jsx
+++ src/map.jsx
@@ -5,6 +5,8 @@ import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import { fromLonLat } from 'ol/proj';
 import Draw from 'ol/interaction/Draw';
+import {Vector as VectorLayer} from 'ol/layer.js';
+import {Vector as VectorSource} from 'ol/source.js';
 
 class SolarMap extends React.Component {
   constructor(props) {
@@ -16,12 +18,22 @@ class SolarMap extends React.Component {
 
   componentDidMount() {
     const bostonCoords = [-71.05, 42.36];
+    const raster = new TileLayer({
+      source: new OSM()
+    });
+    const source = new VectorSource({wrapX: false});
+    const vector = new VectorLayer({
+      source: source
+    });
+    const draw = new Draw({
+      source: source,
+      type: "Polygon"
+    });
     const mapShow = new Map({
       target: 'map',
       layers: [
-        new TileLayer({
-          source: new OSM()
-        })
+        raster,
+        vector
       ],
       view: new View({
         center: fromLonLat(bostonCoords),
@@ -29,6 +41,7 @@ class SolarMap extends React.Component {
       }),
     });
 
+    mapShow.addInteraction(draw);
     this.setState({ map: mapShow });
   }
   render() {
